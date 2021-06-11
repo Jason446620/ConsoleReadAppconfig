95a3f76d9911e2fb65b5b1889529cad4bb173d90
Jason446620 <panshubei@outlook.com>
2021-06-11 16:23:13 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sValue = ConfigurationSettings.AppSettings["BatFile"];
+            string smtpserver = ConfigurationSettings.AppSettings["smtp"];
+            string sharelocation = System.Configuration.ConfigurationSettings.AppSettings["share"];
+            string usercount = System.Configuration.ConfigurationSettings.AppSettings["UserCount"];
+            Console.WriteLine(sValue);
+            Console.WriteLine(smtpserver);
+            Console.WriteLine(sharelocation);
+            Console.WriteLine(usercount);
+            string test = " { \"First line\", \"Second line\", \"Third line啊萨达萨达萨达是\" }";
+            string log = @"D:\home\site\wwwroot\app_data\jobs\continuous\ConsoleApp1\log.txt";
+            using (var sw = new StreamWriter(log))
+            {
+                sw.WriteLine(test);
+            }
+            Console.Read();
+        }
+    }
+}
